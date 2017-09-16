232cb61aace52518aeb6e336535d34cef795a28f
Jacob Wynkoop <jwynkoo@purdue.edu>
2017-09-16 15:31:47 -0400

    App basics
    first commit, basic things, rules menu/buttons for play menu

---
+++ app/src/main/java/com/example/jacob/wizardduel/MainMenu.java
@@ -0,0 +1,36 @@
+package com.example.jacob.wizardduel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+
+        final Button gotoPlay = (Button)findViewById(R.id.gotoPlay);
+        gotoPlay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent goPlay = new Intent(MainMenu.this, PlayScreen.class);
+                startActivity(goPlay);
+            }
+        });
+
+        final Button gotoRules = (Button)findViewById(R.id.gotoRules);
+        gotoRules.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View v) {
+                Intent goRules = new Intent(MainMenu.this, RulesMenu.class);
+                startActivity(goRules);
+            }
+        });
+    }
+
+}
